a30e3c6e821b121ce29ccf6dee1f5f0a99d8668e
Alemayehu <alemayehudabi606@gmail.com>
2025-10-14 22:45:26 +0300

    wip: service nd dto type

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import type { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto: AuthDto) {
     return 'this is sign up api';
   }
   @Post('signin')
-  signin() {
+  signin(@Body() dto: AuthDto) {
     return 'this is sign in api';
   }
 }
