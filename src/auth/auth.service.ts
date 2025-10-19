7da43058c276984c8e7aafcf63ec2a461dc820a8
Alemayehu <alemayehudabi606@gmail.com>
2025-10-20 01:08:36 +0300

    wip: passport jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async signup(dto: AuthDto) {
     const hashed = await argon.hash(dto.password);
 
@@ -49,5 +55,19 @@ export class AuthService {
 
     const { hashedPassword, ...rest } = user;
     return rest;
+  }
+
+  sign_jwt(userId: number, email: string) {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const token = this.jwt.signAsync(payload, {
+      secret: this.config.get('JWT_SECRET'),
+      expiresIn: '15m', // example: token expires in 15 minutes
+    });
+
+    return token;
   }
 }
