b51d280b4d51581a8cb829b4a31859bb8285adee
Alemayehu <alemayehudabi606@gmail.com>
2025-10-20 21:21:10 +0300

    wip: custom guard and validate user

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(config: ConfigService) {
+  constructor(
+    config: ConfigService,
+    private prisma: PrismaService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_ACCESS_SECRET')!,
@@ -14,8 +18,17 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 
   // 👇 REQUIRED METHOD for Passport to validate the token payload
   async validate(payload: { sub: number; email: string }) {
-    // payload is the JWT data (decoded)
-    // Here you can attach data to the request object
-    return { userId: payload.sub, email: payload.email };
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+
+    if (!user) throw new Error('User not found');
+
+    // remove hashedPassword safely
+    const { hashedPassword, ...rest } = user;
+
+    return rest;
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import type { Request } from 'express';
+import { JwtGuard } from 'src/auth/jwtGuard';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getme(@Req() req: Request) {
+    return req.user;
+  }
+}
